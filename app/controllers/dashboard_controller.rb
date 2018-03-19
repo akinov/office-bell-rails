91d33f505b6f52e98ebb04e1b7d7203408d13c9e
akinov <ayumoto10@gmail.com>
2018-03-19 22:19:38 +0900

    認証導線実装

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,16 @@
 class DashboardController < ApplicationController
+  before_action :set_certification_url if current_user.slack_access_token.blank?
+
   def index
+  end
+
+  private
+
+  def set_certification_url
+    @certification_url = URI(SLACK_CERTIFICATION_BAES_URL)
+    @certification_url.query = {
+      client_id: SLACK_CLIENT_ID,
+      scope: 'chat:write:bot'
+    }
   end
 end
