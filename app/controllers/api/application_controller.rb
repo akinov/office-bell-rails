b411ecc3db36fbd425bfcf2e81532e7384aa2638
akinov <ayumoto10@gmail.com>
2018-03-20 12:21:53 +0900

    add Api::ApplicationController

---
+++ app/controllers/api/application_controller.rb
@@ -0,0 +1,46 @@
+class Api::ApplicationController < ActionController::API
+  include AbstractController::Translation
+
+  before_action :authenticate_user_from_token!
+
+  respond_to :json
+
+  ##
+  # User Authentication
+  # Authenticates the user with OAuth2 Resource Owner Password Credentials
+  def authenticate_user_from_token!
+    auth_token = request.headers['Authorization']
+
+    if auth_token
+      authenticate_with_auth_token auth_token
+    else
+      authenticate_error
+    end
+  end
+
+  private
+
+  def authenticate_with_auth_token auth_token
+    unless auth_token.include?(':')
+      authenticate_error
+      return
+    end
+
+    user_id = auth_token.split(':').first
+    user = User.where(id: user_id).first
+
+    if user && Devise.secure_compare(user.access_token, auth_token)
+      # User can access
+      sign_in user, store: false
+    else
+      authenticate_error
+    end
+  end
+
+  ##
+  # Authentication Failure
+  # Renders a 401 error
+  def authenticate_error
+    render json: { error: t('devise.failure.unauthenticated') }, status: 401
+  end
+end
