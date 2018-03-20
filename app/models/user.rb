845d2d0ca644ab7e288973f1a88353d7560ea4e4
akinov <ayumoto10@gmail.com>
2018-03-20 12:20:41 +0900

    Userモデル認証情報生成実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates:authentication_token, uniqueness: true, allow_nil: true
+  after_create :generate_authentication_token
+
+  # 認証トークンが無い場合は作成
+  def ensure_authentication_token
+    self.authentication_token || generate_authentication_token
+  end
+
+  # 認証トークンの作成
+  def generate_authentication_token
+    loop do
+      old_token = self.authentication_token
+      token = SecureRandom.urlsafe_base64(24)
+      break token if (self.update!(authentication_token: token) rescue false) && old_token != token
+    end
+  end
 end
