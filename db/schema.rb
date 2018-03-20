3e2c48d5d2a7f82dc644e747e96f37dfd26f8d9b
akinov <ayumoto10@gmail.com>
2018-03-20 10:09:16 +0900

    api用トークンカラム用意

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319145824) do
+ActiveRecord::Schema.define(version: 20180319153153) do
 
   create_table "users", force: :cascade do |t|
     t.string   "account",                             null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180319145824) do
     t.string   "slack_team_name"
     t.string   "slack_channel_name"
     t.string   "slack_webhook_url"
+    t.string   "authentication_token"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
